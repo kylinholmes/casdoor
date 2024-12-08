7bb78c0f80ecf68f56d3c09d40df85fb809f140b
kylinholmes <zhangyuhao@ft.tech>
2024-12-08 14:07:53 +0800

    fix git

--- routers/authz_filter.go
+++ routers/authz_filter.go
@@ -20,11 +20,11 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/kylinholmes/casdoor/object"
+	"github.com/casdoor/casdoor/object"
 
 	"github.com/beego/beego/context"
-	"github.com/kylinholmes/casdoor/authz"
-	"github.com/kylinholmes/casdoor/util"
+	"github.com/casdoor/casdoor/authz"
+	"github.com/casdoor/casdoor/util"
 )
 
 type Object struct {
